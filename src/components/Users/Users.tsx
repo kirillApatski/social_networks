1162a7b7034359808309bbe773acf82c0f082636
Kirill <apatski94@gmail.com>
2022-12-05 22:43:13 +0300

    refactor

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from "./Users.module.css";
 import {userType} from "../../redax/usersReducer";
-import {NavLink} from "react-router-dom";
+import {User} from "./User";
 
 type UsersPropsType = {
     usersTotalCount: number
@@ -31,47 +31,13 @@ export const Users = (props: UsersPropsType) => {
                                                       }}>{page}</span>)}
             </div>
             {
-                props.users.map(user => {
-                    return (
-                        <div className={styles.userWrapper} key={user.id}>
-                            <div className={styles.userAvatar}>
-                                <NavLink to={`/profile/` + user.id}>
-                                    <img
-                                        src={user.photos.small !== null ? user.photos.small : "https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?b=1&k=20&m=476085198&s=170667a&w=0&h=Ct4e1kIOdCOrEgvsQg4A1qeuQv944pPFORUQcaGw4oI="}
-                                        alt="avatar"/>
-                                </NavLink>
-
-
-                                {
-                                    user.followed
-                                        ? <button disabled={props.followingProgress.some(id => id === user.id)}
-                                                  onClick={() => {
-                                                      props.unfollow(user.id)
-                                                  }
-
-                                                  }>UnFollow</button>
-
-                                        : <button disabled={props.followingProgress.some(id => id === user.id)}
-                                                  onClick={() => {
-                                                      props.follow(user.id)
-                                                  }
-
-                                                  }>Follow</button>
-                                }
-                            </div>
-                            <div className={styles.userDescription}>
-                                <div className={styles.userNameAndStatus}>
-                                    <span className={styles.userFullName}>{user.name}</span>
-                                    <span className={styles.userStatus}>{user.status}</span>
-                                </div>
-                                <div className={styles.userCountryAndCity}>
-                                    <span className={styles.userCountry}>{"user.location.country"}</span>
-                                    <span className={styles.userCity}>{"user.location.city"}</span>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })
+                props.users.map(user => <User
+                    key={user.id}
+                    user={user}
+                    followingProgress={props.followingProgress}
+                    follow={props.follow}
+                    unfollow={props.unfollow}
+                />)
             }
         </div>
     );

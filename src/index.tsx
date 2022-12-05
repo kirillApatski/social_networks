e5999aea365b74dac8f366688b6cc235ba942e69
Kirill <apatski94@gmail.com>
2022-12-05 22:43:28 +0300

    Refactor

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import {store} from "./redax/redux-store";
 import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>,
     document.getElementById('root')
 );
 

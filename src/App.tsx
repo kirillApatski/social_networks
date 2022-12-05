708cdb2ad99f6766fda64d7e415cdc354ac917e6
Kirill <apatski94@gmail.com>
2022-12-05 22:43:30 +0300

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Navigation} from "./components/Navigation/Navigation";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
-import {BrowserRouter,Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
@@ -14,7 +14,6 @@ import {initializeApp} from "./redax/app-reducer";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "./redax/redux-store";
 import {Preloader} from "./components/common/Preloader/Preloader";
-
 
 
 function App() {
@@ -27,33 +26,31 @@ function App() {
     }, [])
 
 
-    if(!initialized) {
+    if (!initialized) {
         return <Preloader/>
     }
 
 
     return (
-        <BrowserRouter>
-            <div className="app-wrapper">
-                <HeaderContainer/>
-                <Navigation/>
-                <div className="appWrapperContent">
-                    <Route path={"/profile/:userId?"}
-                           render={() => <ProfileContainer/>}
-                    />
-                    <Route path={"/dialogs"}
-                           render={() => <DialogsContainer/>}
-                    />
-                    <Route path={"/users"}
-                           render={() => <UsersContainer/>}
-                    />
-                    <Route path={"/news"} render={() => <News/>}/>
-                    <Route path={"/music"} render={() => <Music/>}/>
-                    <Route path={"/settings"} render={() => <Settings/>}/>
-                    <Route path={"/login"} render={() => <Login/>}/>
-                </div>
+        <div className="app-wrapper">
+            <HeaderContainer/>
+            <Navigation/>
+            <div className="appWrapperContent">
+                <Route path={"/profile/:userId?"}
+                       render={() => <ProfileContainer/>}
+                />
+                <Route path={"/dialogs"}
+                       render={() => <DialogsContainer/>}
+                />
+                <Route path={"/users"}
+                       render={() => <UsersContainer/>}
+                />
+                <Route path={"/news"} render={() => <News/>}/>
+                <Route path={"/music"} render={() => <Music/>}/>
+                <Route path={"/settings"} render={() => <Settings/>}/>
+                <Route path={"/login"} render={() => <Login/>}/>
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 

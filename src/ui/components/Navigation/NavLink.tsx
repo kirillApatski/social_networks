c4c21ab508ee6c2858b9910a0b4905a82b0c8ca2
Kirill <apatski94@gmail.com>
2022-12-10 02:34:21 +0300

    added styledComponent NavLink

---
+++ src/ui/components/Navigation/NavLink.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {navLinks} from "../../stylesComponent/Sidebar/navLinksData";
+
+export const NavLink = () => {
+    return (
+        <>
+            {
+                navLinks.map(({id, label, link}) => {
+
+                })
+            }
+        </>
+    );
+};
+

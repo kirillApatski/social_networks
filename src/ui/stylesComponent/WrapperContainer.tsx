62c2e1f8ba4d46e2e30ca63619301209da3da034
Kirill <apatski94@gmail.com>
2022-12-10 02:34:54 +0300

    added styledComponent WrapperContainer

---
+++ src/ui/stylesComponent/WrapperContainer.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const WrapperContainer = styled.div(props => ({
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "20px",
+    width: "100%",
+    maxWidth: "1200px",
+}))

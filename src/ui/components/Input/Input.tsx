b9571531ff35d6fa3e2fc9a92b707e5d1766cc5a
Kirill <apatski94@gmail.com>
2022-12-10 23:19:26 +0300

    added component Input / InputStyled

---
+++ src/ui/components/Input/Input.tsx
@@ -0,0 +1,41 @@
+import React, {DetailedHTMLProps, InputHTMLAttributes} from "react";
+import {InputStyled} from "./InputStyled";
+
+export type TDefaultInputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement>;
+export type TInputProps = TDefaultInputProps & {
+    leftIcon?: React.ReactNode;
+    rightIcon?: React.ReactNode;
+    error?: string;
+    ref?: string;
+};
+
+const Input: React.FC<TInputProps> = ({...props}) => {
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.onChange && props.onChange(e);
+    };
+    const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        props.onBlur && props.onBlur(e);
+    };
+    const onKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        props.onKeyUp && props.onKeyUp(e);
+    };
+
+    return (
+            <InputStyled
+                placeholder={props.placeholder || ""}
+                type={props.type}
+                isError={!!props.error}
+                onChange={onChange}
+                onKeyUp={onKeyUp}
+                name={props.name}
+                value={props.value}
+                onBlur={onBlur}
+                onError={props.onError}
+                hasLeftIcon={!!props.leftIcon}
+                hasRightIcon={!!props.rightIcon}
+            />
+    );
+};
+
+export default Input;
---
+++ src/ui/components/Input/InputStyled.tsx
@@ -0,0 +1,50 @@
+import styled, {css} from "styled-components";
+
+
+interface TSInputProps {
+    isError?: boolean;
+    hasRightIcon?: boolean;
+    hasLeftIcon?: boolean;
+}
+
+export const InputStyled = styled.input<TSInputProps>`
+  padding: 10px 15px;
+  border-radius: 15px;
+  background-color: ${({theme}) => theme.colors.input.default};
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  width: 100%;
+  transition-property: background-color;
+  transition: 0.2s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+  &:focus {
+    border: ${({theme}) => `1px solid ${theme.colors.primary}`};
+    opacity: 1;
+  }
+
+  ${(props) =>
+    props.isError &&
+    css`
+            border: 1px solid ${props.theme.colors.severity.error};
+
+            &:focus {
+              border: 1px solid ${props.theme.colors.severity.error};
+            }
+          `}
+  &::placeholder {
+    opacity: 0.4;
+  }
+
+  ${(props) =>
+    props.hasLeftIcon &&
+    css`
+            padding-left: 40px;
+          `}
+  ${(props) =>
+    props.hasRightIcon &&
+    css`
+            padding-right: 35px;
+          `}
+`;

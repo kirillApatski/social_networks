436b114ebe807ce222f1c1f75ec69b79520fbbd3
Kirill <apatski94@gmail.com>
2022-12-15 23:59:39 +0300

    added updatePhoto

--- src/api/api.ts
+++ src/api/api.ts
@@ -38,7 +38,18 @@ export const profileAPI = {
     },
     updateStatus (status: string) {
         return instance.put(`profile/status`, {status: status})
-    }
+    },
+    updatePhoto (photo: any) {
+        const formData = new FormData()
+        formData.append('image', photo)
+        console.log(formData)
+        return instance
+            .put(`/profile/photo`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+    },
 }
 
 export const authAPI = {

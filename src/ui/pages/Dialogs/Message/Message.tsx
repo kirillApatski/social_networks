e52e3022ea558ebef830aafe5530fdfc6edc623a
Kirill <apatski94@gmail.com>
2022-12-19 20:55:08 +0300

    Fix Ui component

--- src/ui/pages/Dialogs/Message/Message.tsx
+++ src/ui/pages/Dialogs/Message/Message.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {MessagesType} from "../../../../bll/redax/dialogsReducer";
+import {UiWrapper} from "../../../styles/Wrapper";
 
 type MessagesItemPropsType = {
     messages: MessagesType[]
 }
 
 export const Message = (props: MessagesItemPropsType) => {
-    let messageItem = props.messages.map(message => <div key={message.id}>{message.message}</div>)
+    let messageItem = props.messages.map(message => <UiWrapper width={"fit-content"} key={message.id}>{message.message}</UiWrapper>)
     return (
         <>
             {messageItem}

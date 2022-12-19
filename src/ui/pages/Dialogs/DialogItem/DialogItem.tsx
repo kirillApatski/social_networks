3fb53fc1e40227a8b38459921a8dca5f1ee4763a
Kirill <apatski94@gmail.com>
2022-12-19 20:55:02 +0300

    Fix Ui component

--- src/ui/pages/Dialogs/DialogItem/DialogItem.tsx
+++ src/ui/pages/Dialogs/DialogItem/DialogItem.tsx
@@ -1,7 +1,7 @@
-import s from "./DialogItem.module.css";
 import {NavLink} from "react-router-dom";
 import React from "react";
 import {DialogsType} from "../../../../bll/redax/dialogsReducer";
+import {UiWrapper} from "../../../styles/Wrapper";
 
 type DialogsItemsPropsType = {
     dialogs: DialogsType[]
@@ -9,9 +9,11 @@ type DialogsItemsPropsType = {
 
 
 export const DialogItem = (props: DialogsItemsPropsType) => {
+
     let dialogsItem = props.dialogs.map(dialog => {
-        return <div className={s.dialogItem} key={dialog.id}><NavLink to={`/dialogs/${dialog.id}`} activeClassName={s.active}>{dialog.name}</NavLink></div>
+        return <UiWrapper key={dialog.id}><NavLink style={isActive => isActive ? {color: "red"} : {}} to={`/dialogs/${dialog.id}`}>{dialog.name}</NavLink></UiWrapper>
     })
+
     return (
         <>
             {dialogsItem}

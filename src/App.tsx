dae80813fd5eac5a27041bc564a6c425821f6ef4
Kirill <apatski94@gmail.com>
2023-01-03 22:42:39 +0300

    Refactoring Routes to Pages component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import React, {useEffect} from 'react';
 import {Navigation} from "./ui/layout/Navigation/Navigation";
-import {News} from "./ui/pages/News/News";
-import {Music} from "./ui/pages/Music/Music";
-import {Settings} from "./ui/pages/Settings/Settings";
-import {Route} from "react-router-dom";
-import UsersContainer from "./ui/pages/Users/UsersContainer";
-import ProfileContainer from "./ui/pages/Profile/ProfileContainer";
 import HeaderContainer from "./ui/layout/Header/HeaderContainer";
-import {Login} from "./ui/pages/Login/Login";
 import {initializeApp} from "./bll/redax/app-reducer";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "./bll/redax/redux-store";
@@ -15,7 +8,7 @@ import {Preloader} from "./ui/common/Preloader/Preloader";
 import {AppWrapper} from "./ui/styles/AppWrapper";
 import {WrapperContainer} from "./ui/styles/WrapperContainer";
 import {Wrapper} from "./ui/styles/Wrapper";
-import {Dialogs} from "./ui/pages/Dialogs/Dialogs";
+import {Pages} from "./ui/pages/Pages";
 
 
 function App() {
@@ -37,22 +30,7 @@ function App() {
                 <HeaderContainer/>
                 <Navigation/>
                 <Wrapper width={"70%"}>
-                    <Route exact path={"/"}
-                           render={() => <ProfileContainer/>}
-                    />
-                    <Route path={"/profile/:userId?"}
-                           render={() => <ProfileContainer/>}
-                    />
-                    <Route path={"/dialogs"}
-                           render={() => <Dialogs/>}
-                    />
-                    <Route path={"/users"}
-                           render={() => <UsersContainer/>}
-                    />
-                    <Route path={"/news"} render={() => <News/>}/>
-                    <Route path={"/music"} render={() => <Music/>}/>
-                    <Route path={"/settings"} render={() => <Settings/>}/>
-                    <Route path={"/login"} render={() => <Login/>}/>
+                    <Pages/>
                 </Wrapper>
             </WrapperContainer>
         </AppWrapper>

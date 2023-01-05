6b4498d3552e434f40dc53ca36af6053d598531b
Kirill <apatski94@gmail.com>
2023-01-05 21:23:58 +0300

    Delete UsersContainer

--- src/ui/pages/Users/Users.tsx
+++ src/ui/pages/Users/Users.tsx
@@ -1,37 +1,49 @@
-import React from 'react';
-import {userType} from "../../../bll/redax/usersReducer";
+import React, {useEffect} from 'react';
+import {follow, requestUsers, unfollow} from "../../../bll/redax/usersReducer";
 import {User} from "./User";
 import {UiWrapper} from "../../styles/Wrapper";
 import {Pagination} from "../../components/Pagination/Pagination";
+import {useAppSelector} from "../../../hooks/hooks";
+import {useDispatch} from "react-redux";
 
 
-type UsersPropsType = {
-    usersTotalCount: number
-    pagesSize: number
-    currentPage: number
-    onChangePages: (page: number) => void
-    users: Array<userType>
-    follow: (id: number) => void
-    unfollow: (id: number) => void
-    followingProgress: Array<number>
-}
+export const Users = () => {
+    const dispatch = useDispatch()
+    const isFetching = useAppSelector(state => state.usersPages.isFetching)
+    const users = useAppSelector(state => state.usersPages.users)
+    const pagesSize = useAppSelector(state => state.usersPages.pagesSize)
+    const usersTotalCount = useAppSelector(state => state.usersPages.usersTotalCount)
+    const currentPage = useAppSelector(state => state.usersPages.currentPage)
+    const followingProgress = useAppSelector(state => state.usersPages.followingProgress)
 
-export const Users = (props: UsersPropsType) => {
+    const onChangePages = (page: number) => {
+        dispatch(requestUsers(page, pagesSize))
+    }
 
+    const followHandler = (userID: number) => {
+        dispatch(follow(userID))
+    }
+    const unfollowHandler = (userID: number) => {
+        dispatch(unfollow(userID))
+    }
+
+    useEffect(() => {
+        dispatch(requestUsers(currentPage, pagesSize))
+    }, [])
     return (
         <UiWrapper flexDirection={"column"}
                    width={"100%"}>
             <UiWrapper justifyContent={"center"}>
-                <Pagination pageCount={props.pagesSize} totalCount={props.usersTotalCount}
-                            onChangePages={props.onChangePages}/>
+                <Pagination pageCount={pagesSize} totalCount={usersTotalCount}
+                            onChangePages={onChangePages}/>
             </UiWrapper>
             {
-                props.users.map(user => <User
+                users.map(user => <User
                     key={user.id}
                     user={user}
-                    followingProgress={props.followingProgress}
-                    follow={props.follow}
-                    unfollow={props.unfollow}
+                    followingProgress={followingProgress}
+                    follow={followHandler}
+                    unfollow={unfollowHandler}
                 />)
             }
         </UiWrapper>
